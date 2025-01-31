f4dc15cc10ff121707a987af269477e0bc44e208
W2llf3 <jani_kinnunen@outlook.com>
2025-01-31 14:20:32 +0200

    :)

---
+++ week2/teht6.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function declarations
+void printMenu();
+int getUserChoice();
+void rollD6();
+void rollD10();
+
+int main() {
+    int choice;
+
+    // Seed the random number generator
+    srand(time(0));
+
+    do {
+        printMenu();  // Menu
+        choice = getUserChoice();  // Choice
+
+        switch (choice) {
+            case 1:
+                rollD6();  // Roll D6
+            break;
+
+            case 2:
+                rollD10(); // Roll D10
+            break;
+
+            case 3:
+                printf("Thank you for playing. Goodbye!\n");  // Quit message
+            break;
+
+            default:
+                printf("Invalid choice! Please enter a number between 1 and 3.\n");
+            break;
+        }
+    } while (choice != 3); // Check if 3 (exit)
+
+    return 0;
+}
+
+// Menu
+void printMenu() {
+    printf("\nMenu:\n");
+    printf("1. Roll D6 (1-6)\n");
+    printf("2. Roll D10 (1-10)\n");
+    printf("3. Quit\n");
+    printf("Please select an operation (1-3): ");
+}
+
+// Choice function
+int getUserChoice() {
+    int choice;
+    if (scanf("%d", &choice) != 1) {
+        while(getchar() != '\n')  // Clear input buffer
+        choice = 0;  // Set invalid choice
+    }
+    return choice;
+}
+
+// D6 Function
+void rollD6() {
+    int roll = (rand() % 6) + 1;  // Roll 1-6
+    printf("You rolled a D6: %d\n", roll);
+}
+
+// D10 Function
+void rollD10() {
+    int roll = (rand() % 10) + 1;  // Roll 1-10
+    printf("You rolled a D10: %d\n", roll);
+}
