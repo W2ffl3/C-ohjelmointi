eb7693bd4f8551d13b46317b67f5a312fd3ff2ff
W2ffl3 <jani_kinnunen@outlook.com>
2025-03-12 04:20:55 +0200

    Update teht2.c

--- week1/teht2.c
+++ week1/teht2.c
@@ -8,7 +8,6 @@ int main() {
 
     float taxPercentage, higherTaxPercentage, yearlyLimit;
 
-    // Verotiedot.
     printf("Enter the tax percentage: ");
     scanf("%f", &taxPercentage);
 
@@ -18,14 +17,12 @@ int main() {
     printf("Enter the tax percentage for income above the yearly limit: ");
     scanf("%f", &higherTaxPercentage);
 
-    // Palkka kysely.
     printf("Enter the monthly salary for each of the 12 months:\n");
     for (int i = 0; i < NUM_MONTHS; i++) {
         printf("Month %d: ", i + 1);
         scanf("%f", &salaries[i]);
     }
 
-    // Verolasku.
     for (int i = 0; i < NUM_MONTHS; i++) {
         if (salaries[i] <= yearlyLimit) {
             taxes[i] = (salaries[i] * taxPercentage) / 100;
@@ -35,7 +32,6 @@ int main() {
         }
     }
 
-    // Tulostus.
     printf("\n Monthly \t Salaries \t \t Taxes: \n");
     for (int i = 0; i < NUM_MONTHS; i++) {
         printf("Month %d: \t Salary = %.2f \t Tax = %.2f\n", i + 1, salaries[i], taxes[i]);
