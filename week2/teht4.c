7c60a8a68c28dd7b701b71e80566baa2723a15f5
W2ffl3 <jani_kinnunen@outlook.com>
2025-03-12 04:23:21 +0200

    :)

--- week2/teht4.c
+++ week2/teht4.c
@@ -4,13 +4,13 @@ int read_integer(void) {
     int num;
     while (1) {
         if (scanf("%d", &num) == 1) {
-            return num; // Successfully read an integer
+            return num;
         }
 
-        printf("Invalid input\n");  // Error handling for invalid input
+        printf("Invalid input\n");
         printf("Enter positive number. Negative to stop: ");
 
-        while (getchar() != '\n');  // Clear input buffer
+        while (getchar() != '\n');
     }
 }
 
@@ -20,19 +20,19 @@ int main() {
     printf("Enter positive number. Negative to stop: ");
 
     while (1) {
-        number = read_integer(); // Read an integer
+        number = read_integer();
 
         if (number < 0) {
-            break; // End if negative
+            break;
         }
 
-        sum += number; // Add the number to the sum
-        count++; // Increment the count of positive numbers
+        sum += number;
+        count++;
 
         printf("Enter positive number. Negative to stop: ");
     }
 
-    if (count > 0) {    // Calculate and display the average
+    if (count > 0) {
         double average = (double)sum / count;
         printf("You entered %d positive numbers. The average is: %.3f\n", count, average);
     } else {
